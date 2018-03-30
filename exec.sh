9ae95e6824b07a709badf6ed93f914ca3c5cfb0e
Alessandro Steri <alessandro.steri@gmail.com>
2018-03-31 01:23:36 +0200

    add script to launch

---
+++ exec.sh
@@ -0,0 +1,6 @@
+
+
+
+
+####  batch_size, embedding_size, window_size, neg_samples, vocabulary_size, num_domain_words, num_steps, learning_rate
+python word2vec.py 32 128 1 5 15000 800000 400000 0.5
